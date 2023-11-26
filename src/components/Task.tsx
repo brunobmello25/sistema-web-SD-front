4ccd05fc94530f3a4a1342540bd6fae0a78a1b73
Bruno Mello <bruno.barros.mello@gmail.com>
2023-11-25 23:33:42 -0300

    feat: mark task as done

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,6 +2,10 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 import { useModal } from "~/context/modal-context";
 import { EditTaskModal } from "./EditTaskModal";
 import { type Task } from "~/protocols";
+import { useState } from "react";
+import { useMutation } from "react-query";
+import { updateTask } from "~/services/update-task";
+import { useAppContext } from "~/context/app-context";
 
 type Props = {
   task: Task;
@@ -10,10 +14,37 @@ type Props = {
 
 export function Task({ onTaskUpdated, task }: Props) {
   const { setModal } = useModal();
+  const { api } = useAppContext();
+  const [checked, setChecked] = useState(task.done);
+
+  const updateCheckedMutation = useMutation((checked: boolean) =>
+    updateTask({ done: checked, title: task.title, taskId: task.id }, api),
+  );
+
+  function handleOnCheck() {
+    const originalStatus = task.done;
+
+    setChecked(!originalStatus);
+
+    updateCheckedMutation
+      .mutateAsync(!originalStatus)
+      .then(() => {
+        onTaskUpdated();
+      })
+      .catch(() => {
+        alert("Erro ao atualizar tarefa. Por favor tente novamente.");
+        setChecked(originalStatus);
+      });
+  }
 
   return (
     <li className="flex items-center justify-between rounded bg-gray-600 p-2">
-      <input type="checkbox" className="mr-2 h-4 w-4" />
+      <input
+        type="checkbox"
+        className="mr-2 h-4 w-4"
+        checked={checked}
+        onChange={handleOnCheck}
+      />
 
       <span className="flex-grow text-white">{task.title}</span>
       <div className="flex items-center space-x-2">
